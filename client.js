13fed2db533068405e64ae1745eb772ac7cd5d4e
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-11 20:09:22 +0000

    Added connection event handlers

--- client.js
+++ client.js
@@ -13,6 +13,15 @@ const connect = function() {
   conn.on("data", (data) => {
     console.log(data);
   });
+  
+  conn.on("connect", ()=>{
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: SHR');
+  });
+  
   return conn;
 };
 
